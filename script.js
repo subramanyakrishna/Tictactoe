9d04f1a3f30de9bee8cdb521e1c530a1594df38f
kulalsk2 <subramanyakulal2000@gmail.com>
2020-08-22 22:23:40 +0530

    UI completed Logic is remaining

---
+++ script.js
@@ -0,0 +1,18 @@
+let Board;
+const Human = '0';
+const Robot = 'x';
+const winning_combinaion=[
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6], 
+    [3,4,5],
+    [6,7,8]
+];
+
+const cells = document.querySelectorAll(".cell");
+function starttheGame(){
+
+}
